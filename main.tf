7a6cd8a8bf1eabfe2c1611540f49db169f0490ef
Arthofer <56601366+Arthofer@users.noreply.github.com>
2020-06-06 17:13:22 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "space"
+    organization = "cdunlap"
 
     workspaces {
       name = "aws-sentinel-demo"
@@ -13,11 +13,19 @@ resource "random_pet" "server" {
 }
 
 provider "aws" {
-  access_key = "AKIAJJUBLB6N43T67RMA"
-  secret_key = "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"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 
   #don't change this from us-west-2 :)
   region = "us-west-2"
+}
+
+variable "aws_access_key" {
+  description = "access key"
+}
+
+variable "aws_secret_key" {
+  description = "secret key"
 }
 
 data "aws_ami" "ubuntu" {
@@ -43,10 +51,38 @@ resource "aws_instance" "demo" {
    #instance_type = "t2.xlarge"
    instance_type = "t2.micro"
 
+  key_name = var.ssh_key_name
+
   tags = {
     Name = random_pet.server.id
     #uncomment this for working, comment out for sentinel policy trigger
     Owner = "chrisd"
     TTL   = "24hrs"
   }
+  user_data = data.template_file.cloud-init.rendered
+}
+
+output "private_ip" {
+  description = "Private IP of instance"
+  value       = join("", aws_instance.demo.*.private_ip)
+}
+
+output "public_ip" {
+  description = "Public IP of instance (or EIP)"
+  value       = join("", aws_instance.demo.*.public_ip)
+}
+
+data "template_file" "cloud-init" {
+  template = file("cloud-init.tpl")
+
+  vars = {
+    boinc_project_id = var.boinc_project_id
+  }
+}
+
+variable "boinc_project_id" {
+  description = "Boinc Project id: boinccmd --lookup_account URL email password https://boinc.berkeley.edu/wiki/Boinccmd_tool"
+}
+variable "ssh_key_name" {
+  description = "You AWS SSH KeyName"
 }
