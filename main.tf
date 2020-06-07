291e9d08adb0c626eec3905ef2dac09c2529f7db
Arthofer <56601366+Arthofer@users.noreply.github.com>
2020-06-06 19:02:33 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,12 +10,14 @@ terraform {
 }
 
 resource "random_pet" "server" {
-}
-
-resource "aws_key_pair" "deployer" {
+resource "aws_key_pair" "deployer"
   key_name   = "austindemoSSH"
   public_key = "02001ce6056d38dc5"
-  }
+}
+
+resource "aws_key_pair" "deployer"
+  key_name   = "austindemoSSH"
+  public_key = "02001ce6056d38dc5"
 
 provider "aws" {
   access_key = var.aws_access_key
